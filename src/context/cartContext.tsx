57d0946089297300589594eafdb7e20162b8e97f
Maryam Nasir <maryamnasir555@yahoo.com>
2024-10-06 11:50:19 +0500

    Add Cart context

---
+++ src/context/cartContext.tsx
@@ -0,0 +1,66 @@
+import { createContext, useState } from "react";
+
+type CartProductType = {
+  id: string;
+  quantity: number;
+};
+
+type CartType = {
+  products: CartProductType[];
+  onIncrement: (id: string) => void;
+  onDecrement: (id: string) => void;
+};
+
+const INITIAL_CART_CONTEXT: CartType = {
+  products: [],
+  onIncrement: (id: string) => {},
+  onDecrement: (id: string) => {},
+};
+
+export const CartContext = createContext<CartType>(INITIAL_CART_CONTEXT);
+
+const CartContextProvider = ({ children }: { children: React.ReactNode }) => {
+  const [cartProducts, setCartProducts] = useState<CartProductType[]>([]);
+
+  const handleIncrement = (productId: string) => {
+    const updatedCartProducts: CartProductType[] = [...cartProducts];
+    const index: number = updatedCartProducts.findIndex(
+      (product) => product.id === productId
+    );
+    if (index === -1) {
+      const cartProduct: CartProductType = {
+        id: productId,
+        quantity: 1,
+      };
+      updatedCartProducts.push(cartProduct);
+    } else {
+      updatedCartProducts[index].quantity += 1;
+    }
+    setCartProducts(updatedCartProducts);
+  };
+
+  const handleDecrement = (productId: string) => {
+    const updatedCartProducts: CartProductType[] = [...cartProducts];
+    const index: number = updatedCartProducts.findIndex(
+      (product) => product.id === productId
+    );
+    updatedCartProducts[index].quantity -= 1;
+
+    if (updatedCartProducts[index].quantity === 0) {
+      updatedCartProducts.splice(index, 1);
+    }
+    setCartProducts(updatedCartProducts);
+  };
+
+  const cartContext: CartType = {
+    products: cartProducts,
+    onIncrement: handleIncrement,
+    onDecrement: handleDecrement,
+  };
+
+  return (
+    <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
+  );
+};
+
+export default CartContextProvider;
