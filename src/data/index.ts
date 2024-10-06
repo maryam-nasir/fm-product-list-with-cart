604748fa05ab601bf6d89a38ff35c9f21c119953
Maryam Nasir <maryamnasir555@yahoo.com>
2024-10-06 11:50:32 +0500

    Add product ids to keep track in cart

--- src/data/index.ts
+++ src/data/index.ts
@@ -7,6 +7,7 @@ export type ProductType = {
     tablet: string;
     desktop: string;
   };
+  id: string;
   name: string;
   category: string;
   price: number;
@@ -20,6 +21,7 @@ export const PRODUCTS: ProductType[] = [
       tablet: IMAGE_ASSETS.WAFFLE_TABLET_IMAGE,
       desktop: IMAGE_ASSETS.WAFFLE_DESKTOP_IMAGE,
     },
+    id: "waffle",
     name: "Waffle with Berries",
     category: "Waffle",
     price: 6.5,
@@ -31,6 +33,7 @@ export const PRODUCTS: ProductType[] = [
       tablet: IMAGE_ASSETS.CREME_BRULEE_TABLET_IMAGE,
       desktop: IMAGE_ASSETS.CREME_BRULEE_DESKTOP_IMAGE,
     },
+    id: "vanilla-bean-creme-brulee",
     name: "Vanilla Bean Crème Brûlée",
     category: "Crème Brûlée",
     price: 7.0,
@@ -42,6 +45,7 @@ export const PRODUCTS: ProductType[] = [
       tablet: IMAGE_ASSETS.MACARON_TABLET_IMAGE,
       desktop: IMAGE_ASSETS.MACARON_DESKTOP_IMAGE,
     },
+    id: "macaron",
     name: "Macaron Mix of Five",
     category: "Macaron",
     price: 8.0,
@@ -53,6 +57,7 @@ export const PRODUCTS: ProductType[] = [
       tablet: IMAGE_ASSETS.TIRAMISU_TABLET_IMAGE,
       desktop: IMAGE_ASSETS.TIRAMISU_DESKTOP_IMAGE,
     },
+    id: "tiramisu",
     name: "Classic Tiramisu",
     category: "Tiramisu",
     price: 5.5,
@@ -64,6 +69,7 @@ export const PRODUCTS: ProductType[] = [
       tablet: IMAGE_ASSETS.BAKLAVA_TABLET_IMAGE,
       desktop: IMAGE_ASSETS.BAKLAVA_DESKTOP_IMAGE,
     },
+    id: "baklava",
     name: "Pistachio Baklava",
     category: "Baklava",
     price: 4.0,
@@ -75,6 +81,7 @@ export const PRODUCTS: ProductType[] = [
       tablet: IMAGE_ASSETS.MERINGUE_TABLET_IMAGE,
       desktop: IMAGE_ASSETS.MERINGUE_DESKTOP_IMAGE,
     },
+    id: "lemon-pie",
     name: "Lemon Meringue Pie",
     category: "Pie",
     price: 5.0,
@@ -86,6 +93,7 @@ export const PRODUCTS: ProductType[] = [
       tablet: IMAGE_ASSETS.CAKE_TABLET_IMAGE,
       desktop: IMAGE_ASSETS.CAKE_DESKTOP_IMAGE,
     },
+    id: "red-velvet-cake",
     name: "Red Velvet Cake",
     category: "Cake",
     price: 4.5,
@@ -97,6 +105,7 @@ export const PRODUCTS: ProductType[] = [
       tablet: IMAGE_ASSETS.BROWNIE_TABLET_IMAGE,
       desktop: IMAGE_ASSETS.BROWNIE_DESKTOP_IMAGE,
     },
+    id: "brownie",
     name: "Salted Caramel Brownie",
     category: "Brownie",
     price: 4.5,
@@ -108,6 +117,7 @@ export const PRODUCTS: ProductType[] = [
       tablet: IMAGE_ASSETS.PANNA_COTTA_TABLET_IMAGE,
       desktop: IMAGE_ASSETS.PANNA_COTTA_DESKTOP_IMAGE,
     },
+    id: "panna-cotta",
     name: "Vanilla Panna Cotta",
     category: "Panna Cotta",
     price: 6.5,
