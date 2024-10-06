18c1bad6871fcea6b6b4bf681f3e199d3f82bf5f
Maryam Nasir <maryamnasir555@yahoo.com>
2024-10-06 17:42:58 +0500

    Add Button component

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,18 @@
+type ButtonProps = {
+  label: string;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+  className?: string;
+};
+
+const Button = ({ label, onClick, className = "" }: ButtonProps) => {
+  return (
+    <button
+      className={`w-full h-10 flex items-center justify-center rounded-full text-sm font-semibold text-white bg-red hover:bg-redHover ${className}`}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Button;
