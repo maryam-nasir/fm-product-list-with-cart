96a9b9e54f5b51e77c31f2bd5cb7826d6e2a53fb
Maryam Nasir <maryamnasir555@yahoo.com>
2024-10-06 21:09:47 +0500

    Add root level App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
+import { useState } from "react";
+import Cart from "./components/Cart";
+import Modal from "./components/common/Modal";
+import ProductListing from "./components/ProductListing";
+import ConfirmOrderModal from "./components/ConfirmOrderModal";
+import CartContextProvider from "./context/cartContext";
+
 const App = () => {
-  return <div>Hello World</div>;
+  const [orderConfirmed, setOrderConfirmed] = useState<boolean>(false);
+
+  const handleConfirmOrder = () => {
+    setOrderConfirmed(true);
+  };
+
+  const handleCloseModal = () => {
+    setOrderConfirmed(false);
+  };
+
+  return (
+    <>
+      <CartContextProvider>
+        <div className="flex flex-col md:flex-row">
+          <ProductListing />
+          <Cart onOrderConfirm={handleConfirmOrder} />
+        </div>
+        <Modal isOpen={orderConfirmed}>
+          <ConfirmOrderModal handleReset={handleCloseModal} />
+        </Modal>
+      </CartContextProvider>
+    </>
+  );
 };
 
 export default App;
