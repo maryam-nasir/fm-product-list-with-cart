751247d5366dbf2044b3322395197635c02ed77f
Maryam Nasir <maryamnasir555@yahoo.com>
2024-10-07 10:38:59 +0500

    Add focus states for interactive elements

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -7,7 +7,7 @@ type ButtonProps = {
 const Button = ({ label, onClick, className = "" }: ButtonProps) => {
   return (
     <button
-      className={`w-full h-10 flex items-center justify-center rounded-full text-sm font-semibold text-white bg-red hover:bg-redHover ${className}`}
+      className={`w-full h-10 flex items-center justify-center rounded-full text-sm font-semibold text-white bg-red hover:bg-redHover focus:outline-0 focus:bg-redHover ${className}`}
       onClick={onClick}
     >
       {label}
